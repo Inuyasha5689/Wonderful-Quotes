391bb2fd0090ca956360b5a472cfba949d5c996b
Drake Scott <dascott0204@gmail.com>
2017-02-24 21:17:27 -0600

    I have passed made all the quote components and the data needed for each component. Which allows them to create quotes and for them to be displayed and I have just finished the deleteQuote method using event listeners.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,40 @@
 <template>
     <div class="container">
-    
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+        <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">
+                    Info: click on a Quote to delete it!
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
+
     export default {
-        
+        data: function() {
+            return {
+                quotes: ['My favorite Quote!'],
+                maxQuotes: 10
+            }
+        },
+        methods: {
+            newQuote(quote) {
+                this.quotes.push(quote);
+            },
+            deleteQuote(index) {
+                this.quotes.splice(index, 1);
+            }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote
+        }
     }
 </script>
 
